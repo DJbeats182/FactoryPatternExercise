d5ca521e9b0b9d140cbf023c75ad0ca977a5d9f1
DJ Bates <blooviper@gmail.com>
2022-08-24 10:36:59 -0600

    First Commit

---
+++ FactoryPatternExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using FactoryPatternExercise;
+
+Console.WriteLine("Enter the amount of tires you would like your vehicle to have:");
+
+bool input;
+int wheelCount;
+do
+{
+     input = int.TryParse(Console.ReadLine(), out wheelCount);
+
+    if (!input)
+    {
+        Console.WriteLine("Sorry input not recognized, Please try again:");
+    }
+} while (!input);
+
+var vehicle = VehicleFactory.GetVehicle(wheelCount);
+vehicle.Drive();
---
+++ FactoryPatternExercise/ThreeWheeler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise
+{
+    internal class ThreeWheeler : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("You're going to have a lot of fun with your new Three Wheeler!");
+        }
+    }
+}
